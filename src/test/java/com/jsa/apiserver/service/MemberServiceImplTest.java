e16f7e9f9c319e67dc53d8d056d4ff9cc2177c47
jeongsikan <ajs15010120@gmail.com>
2022-10-06 13:21:45 +0900

    [테스트] Service Test02 서비스 테스트가 빈등록 하기까지 마이바티스 라이브러리추가 .http추가

--- src/test/java/com/jsa/apiserver/service/MemberServiceImplTest.java
+++ src/test/java/com/jsa/apiserver/service/MemberServiceImplTest.java
@@ -1,13 +1,19 @@
 package com.jsa.apiserver.service;
 
+import com.jsa.apiserver.model.dto.RequestMemberDto;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceImplTest {
+    @Mock
+    RequestMemberDto _requestMember;
 
     @Test
     void selectMember() {
+        //_requestMember
     }
 
     @Test
